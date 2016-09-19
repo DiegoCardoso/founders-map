922e419c38339bce362f3bc243449993c66efde9
Diego Cardoso <diegopc86@gmail.com>
2016-09-19 16:11:28 -0300

    Create function in <MainContainer /> to pass to <FoundersForm />
    Add some styling for custom fonts

--- src/containers/MainContainer/index.js
+++ src/containers/MainContainer/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
+import './index.css'
+
+import FoundersForm from '../../components/FoundersForm';
+
 class MainContainer extends React.Component {
+
+  state = {
+    header: null,
+    companies: [],
+  }
+
+  onFoundersFormSubmit = (header, companies) => {
+    this.setState({
+      header,
+      companies,
+    });
+  }
+
   render () {
     return (
-      <h1>
-        Founder's Map
-      </h1>
+      <div className="main-container">
+        <div className="controls-container">
+          <header className="controls-container__header">
+            <h1>
+              Founder's Map
+            </h1>
+          </header>
+          { !this.state.header && <FoundersForm onFoundersFormSubmit={this.onFoundersFormSubmit}/>}
+          { this.state.header && <h2>Foi aqui</h2>}
+        </div>
+        <div className="map-container">
+
+        </div>
+      </div>
     );
   }
 }

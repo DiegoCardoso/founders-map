15bafc3d58818498d5704d352c744d2e65a0b159
Diego Cardoso <diegopc86@gmail.com>
2016-09-20 00:27:17 -0300

    Change component <FilterTableImageCell />
    The change is intended to make the component give the correct
    feedback while the image is loading or in case the link is broken

--- src/components/FilterTable/index.js
+++ src/components/FilterTable/index.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 
+const imageRegex = /^http[s]?:\/\/.*(\.(jpg|png|gif))$/g;
+const siteRegex = /^http[s]?:\/\/.*$/g;
 
 export const FilterTableHeader = ({value, label}) => (
   <th value={value}>
@@ -7,11 +9,83 @@ export const FilterTableHeader = ({value, label}) => (
   </th>
 );
 
-const FilterTableCell = ({value, label}) => (
-  <td value={value}>
-    {label}
-  </td>
-);
+class FilterTableImageCell extends React.Component {
+
+  state = {
+    loading: true,
+    error: false,
+    source: '',
+  }
+
+  componentDidMount() {
+    const image = new Image();
+    image.onerror = () => {
+      this.setState({
+        error: true,
+        loading: false,
+      });
+    }
+    image.onload = () => {
+      this.setState({
+        loading: false,
+        source: this.props.label,
+      });
+    }
+
+    image.src = this.props.label;
+  }
+
+  render() {
+    const { value } = this.props;
+
+    if(this.state.loading) {
+      return (
+        <td>
+          ...
+        </td>
+      );
+    }
+
+    if (this.state.error) {
+      return (
+        <td>
+          no image
+        </td>
+      );
+    }
+    return (
+      <td value={value}>
+        <img width="100px" src={this.state.source} alt="Company's foundation"/>
+      </td>
+    )
+  }
+};
+
+const FilterTableLinkCell = ({value, label}) => {
+  return (
+    <td value={value}>
+      <a href={label} target="_blank">site</a>
+    </td>
+  )
+};
+
+const FilterTableCell = ({value, label}) => {
+  if(imageRegex.exec(label)) {
+    imageRegex.exec('');
+    return <FilterTableImageCell value={value} label={label}/>
+  }
+
+  if(siteRegex.exec(label)) {
+    siteRegex.exec('');
+    return <FilterTableLinkCell value={value} label={label}/>
+  }
+
+  return (
+    <td value={value}>
+      {label}
+    </td>
+  );
+};
 
 const FilterTableRow = ({ row, columns}) => {
   if (!columns || columns.length === 0) {
@@ -32,7 +106,7 @@ class FilterTable extends React.Component {
     const columnsValues = React.Children.map(this.props.children, ({ props }) => {
       return props.value;
     });
-    console.log(columnsValues);
+
     return (
       <table>
         <thead>
